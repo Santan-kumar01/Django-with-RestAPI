f3a33d959e786959f1aec61e41c0f365d4077cd8
Santan Kumar <your_github_email@gmail.com>
2026-03-08 15:27:59 +0530

    till Day-27 filters

---
+++ DAY-27/filterproject/testapp/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class FilterModel(models.Model):
+    name = models.CharField(max_length=30)
+    subject = models.CharField(max_length=50)
+    dept = models.CharField(max_length=40)
+    date = models.DateField()
+# Create your models here.
